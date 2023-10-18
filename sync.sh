ef772e4945fbab743aa8390dcd0b8ddd49c2e055
huynhtuan17ti <huynhminhtuan6429@gmail.com>
2023-10-18 23:28:33 +0700

    add sync script

---
+++ sync.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+cat ~/.vimrc >  .vimrc
+cat ~/.vim/colors/xoria256.vim > colors/xoria256.vim
+cat ~/.vim/bundle/YouCompleteMe/third_party/ycmd/.ycm_extra_conf.py > .ycm_extra_conf.py
